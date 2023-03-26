d8ce44957f82c36cfd85c8c0ec466902ea9657ec
Myobbb <108356919+Myobbb@users.noreply.github.com>
2023-03-26 19:49:29 +0300

    Update bybit_exchange.py

--- hummingbot/connector/exchange/bybit/bybit_exchange.py
+++ hummingbot/connector/exchange/bybit/bybit_exchange.py
@@ -111,7 +111,7 @@ class BybitExchange(ExchangePyBase):
         return self._trading_required
 
     def supported_order_types(self):
-        return [OrderType.LIMIT, OrderType.LIMIT, OrderType.LIMIT_MAKER]
+        return [OrderType.MARKET, OrderType.LIMIT, OrderType.LIMIT_MAKER]
 
     def _is_request_exception_related_to_time_synchronizer(self, request_exception: Exception):
         error_description = str(request_exception)
