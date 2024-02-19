83f0d0cadd43de49ade2896bb965b36d656fe51f
Myobbb <108356919+Myobbb@users.noreply.github.com>
2024-02-19 11:36:59 +0300

    Update bybit_exchange.py

--- hummingbot/connector/exchange/bybit/bybit_exchange.py
+++ hummingbot/connector/exchange/bybit/bybit_exchange.py
@@ -303,10 +303,10 @@ class BybitExchange(ExchangePyBase):
 
                 retval.append(
                     TradingRule(trading_pair,
-                                min_order_size=Decimal(min_order_size)*0,
-                                min_price_increment=Decimal(min_price_increment)*0,
-                                min_base_amount_increment=Decimal(min_base_amount_increment)*0,
-                                min_notional_size=Decimal(min_notional_size)))*0
+                                min_order_size=Decimal(min_order_size)/10000,
+                                min_price_increment=Decimal(min_price_increment)/10000,
+                                min_base_amount_increment=Decimal(min_base_amount_increment)/10000,
+                                min_notional_size=Decimal(min_notional_size)))/10000
 
             except Exception:
                 self.logger().exception(f"Error parsing the trading pair rule {rule.get('name')}. Skipping.")
