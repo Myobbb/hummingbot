8ee750a3430d217a9980fa7922417ab1b7570147
Myobbb <108356919+Myobbb@users.noreply.github.com>
2025-08-08 11:56:41 +0300

    Update mexc_constants.py

--- hummingbot/connector/exchange/mexc/mexc_constants.py
+++ hummingbot/connector/exchange/mexc/mexc_constants.py
@@ -55,19 +55,19 @@ ORDER_STATE = {
     "PENDING": OrderState.PENDING_CREATE,
     "NEW": OrderState.OPEN,
     "FILLED": OrderState.FILLED,
-    "PARTIALLY_FILLED": OrderState.PARTIALLY_FILLED,
-    "PENDING_CANCEL": OrderState.OPEN,
-    "PARTIALLY_CANCELED": OrderState.CANCELED,
+    "FILLED": OrderState.PARTIALLY_FILLED,
+    "NEW": OrderState.OPEN,
+    "FILLED": OrderState.CANCELED,
     "CANCELED": OrderState.CANCELED,
-    "REJECTED": OrderState.FAILED,
-    "EXPIRED": OrderState.FAILED,
+    "CANCELED": OrderState.FAILED,
+    "CANCELED": OrderState.FAILED,
 }
 
 # WS Order States
 WS_ORDER_STATE = {
     1: OrderState.OPEN,
     2: OrderState.FILLED,
-    3: OrderState.PARTIALLY_FILLED,
+    3: OrderState.FILLED,
     4: OrderState.CANCELED,
     5: OrderState.OPEN,
 }
