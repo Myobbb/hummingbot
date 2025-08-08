fc0fd06ee576965560ae879c7fa1024a29b82575
Myobbb <108356919+Myobbb@users.noreply.github.com>
2025-08-08 11:55:45 +0300

    Update mexc_utils.py

--- hummingbot/connector/exchange/mexc/mexc_utils.py
+++ hummingbot/connector/exchange/mexc/mexc_utils.py
@@ -1,17 +1,17 @@
 from decimal import Decimal
 from typing import Any, Dict
 
-from pydantic import ConfigDict, Field, SecretStr
+from pydantic import Field, SecretStr
 
-from hummingbot.client.config.config_data_types import BaseConnectorConfigMap
+from hummingbot.client.config.config_data_types import BaseConnectorConfigMap, ClientFieldData
 from hummingbot.core.data_type.trade_fee import TradeFeeSchema
 
 CENTRALIZED = True
 EXAMPLE_PAIR = "ZRX-ETH"
 
 DEFAULT_FEES = TradeFeeSchema(
-    maker_percent_fee_decimal=Decimal("0.0005"),
-    taker_percent_fee_decimal=Decimal("0.0005"),
+    maker_percent_fee_decimal=Decimal("0.000"),
+    taker_percent_fee_decimal=Decimal("0.000"),
     buy_percent_fee_deducted_from_returns=True
 )
 
@@ -27,26 +27,28 @@ def is_exchange_information_valid(exchange_info: Dict[str, Any]) -> bool:
 
 
 class MexcConfigMap(BaseConnectorConfigMap):
-    connector: str = "mexc"
+    connector: str = Field(default="mexc", const=True, client_data=None)
     mexc_api_key: SecretStr = Field(
         default=...,
-        json_schema_extra={
-            "prompt": "Enter your Mexc API key",
-            "is_secure": True,
-            "is_connect_key": True,
-            "prompt_on_new": True,
-        }
+        client_data=ClientFieldData(
+            prompt=lambda cm: "Enter your Mexc API key",
+            is_secure=True,
+            is_connect_key=True,
+            prompt_on_new=True,
+        )
     )
     mexc_api_secret: SecretStr = Field(
         default=...,
-        json_schema_extra={
-            "prompt": "Enter your Mexc API secret",
-            "is_secure": True,
-            "is_connect_key": True,
-            "prompt_on_new": True,
-        }
+        client_data=ClientFieldData(
+            prompt=lambda cm: "Enter your Mexc API secret",
+            is_secure=True,
+            is_connect_key=True,
+            prompt_on_new=True,
+        )
     )
-    model_config = ConfigDict(title="mexc")
+
+    class Config:
+        title = "mexc"
 
 
 KEYS = MexcConfigMap.construct()
