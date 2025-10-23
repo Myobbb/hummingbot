c46952ed337c17720ff5d137a741730f2bb9af82
Myobbb <108356919+Myobbb@users.noreply.github.com>
2025-10-23 09:54:27 +0300

    adapt_cex_upd

--- hummingbot/connector/exchange/paper_trade/paper_trade_exchange.pyx
+++ hummingbot/connector/exchange/paper_trade/paper_trade_exchange.pyx
@@ -5,7 +5,7 @@ import math
 import random
 from collections import defaultdict, deque
 from decimal import Decimal, ROUND_DOWN
-from typing import Callable, Dict, List, Optional, Tuple,
+from typing import Callable, Dict, List, Optional, Tuple
 
 from cpython cimport PyObject
 from cython.operator cimport address, dereference as deref, postincrement as inc
