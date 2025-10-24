496e4069989886667b34786c053aa208b68d7c1b
Myobbb <108356919+Myobbb@users.noreply.github.com>
2025-10-24 08:19:46 +0300

    okx_conn_upd

--- hummingbot/connector/exchange/okx/okx_exchange.py
+++ hummingbot/connector/exchange/okx/okx_exchange.py
@@ -34,7 +34,8 @@ class OkxExchange(ExchangePyBase):
                  trading_required: bool = True,
                  balance_asset_limit: Optional[Dict[str, Dict[str, Decimal]]] = None,
                  rate_limits_share_pct: Decimal = Decimal("100"),
-                 okx_registration_sub_domain: str = "www"):
+                 okx_registration_sub_domain: str = "www",
+                 **kwargs):
         """
         :param okx_registration_sub_domain: The subdomain to use - options are "www" (default), "app" (US users), or "my" (EEA users)
                               See: https://github.com/ccxt/ccxt/issues/24601
